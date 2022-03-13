6a8b7dd238e8b3ebef30babc3d2de352da48aff0
Mostafa Refaat <mostafarefaat.eladl@gmail.com>
2022-03-14 01:11:48 +0200

    Adding Angular

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ClinicManagementSystem
+
+# Angular BootStrap : 
+
+* this Empty project is angular project with all the dependencies you will need to build well - responsive application . 
+
+* make sure that u called : `npm install` 
---
+++ responsive-client/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery'; // that's is the syntax for jquery
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(){
+
+  }
+
+}
