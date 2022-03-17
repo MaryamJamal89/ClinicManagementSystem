753a1bac1eb006ee0a6afdfcdeb46fc7288911b0
RandaHadad <randahada99@gmail.com>
2022-03-17 13:26:48 +0200

    jj

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+
+const appointment = require('./database/models/appointment');
+const clinic = require('./database/models/clinic');
+const doctor = require('./database/models/doctor');
+const patient = require('./database/models/patient');
+const prescription = require('./database/models/prescription');
+const receptionist = require('./database/models/receptionist');
 
 const server = express();
 
