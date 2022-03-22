8f0f18ad7cde339174ec59c6ad78c45f138c23b6
Mostafa Refaat <mostafarefaat.eladl@gmail.com>
2022-03-22 18:09:51 +0200

    Adding delete

--- Angular/src/app/doctor/doctor-dashboard/doctor-dashboard.component.ts
+++ Angular/src/app/doctor/doctor-dashboard/doctor-dashboard.component.ts
@@ -63,6 +63,7 @@ export class DoctorDashboardComponent implements OnInit {
             eventClick: this.handleEventClick.bind(this),
             eventsSet: this.handleEvents.bind(this),
             events:this.arr, 
+            
             // [
             //   {
             //     title:this.appointments[0].service.name,
@@ -106,20 +107,24 @@ export class DoctorDashboardComponent implements OnInit {
 
     if (title) {
       // this.newAppointment.serviceName=title
-      calendarApi.addEvent(
-
-        {
-          title,
-          start: selectInfo.startStr,
-          end: selectInfo.endStr,
-          allDay: selectInfo.allDay,
-        });
+    
         this.newAppointment.service.name = title 
         this.newAppointment.startDate =  new Date(selectInfo.start)
         this.newAppointment.endDate =  new Date(selectInfo.end)
         
         this.docSrv.addAppointment(this.newAppointment).subscribe({
-          next:a=>{this.newAppointment=a}
+          next:a=>{this.newAppointment=a
+          console.log(this.newAppointment)
+console.log(this.newAppointment._id)
+            calendarApi.addEvent(
+              {
+                title,
+                start: selectInfo.startStr,
+                end: selectInfo.endStr,
+                allDay: selectInfo.allDay,
+                id:this.newAppointment._id,
+              });
+          }
         })
         
       //this.docSrv.addAppointment(new Appointment("0","0", "0", new Date(selectInfo.startStr), 1, paymentMethod, fees, title));
