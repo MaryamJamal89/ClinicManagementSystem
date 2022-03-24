eb79d27e36c9b11a5d129d236c4c7a73e0c91a29
RandaHadad <randahada99@gmail.com>
2022-03-24 11:17:30 +0200

    backend

--- BackEnd/Controllers/clinic_controller.js
+++ BackEnd/Controllers/clinic_controller.js
@@ -2,32 +2,33 @@
 const Clinic = require("./../Models/clinic");
 
 ////////////////////////////////////////GET///////////////////////////////////////////////////////////
-exports.getClinic = function(request, response, next) {
-    Clinic.find({})
-        .then(result => {
-            response.status(200).json(result)
-        })
-        .catch(error => next(error));
-}
-exports.getServiceName = function(request, response, next) {
-    Clinic.findOne({},{
-        services:{$elemMatch:{_id : request.body.serviceid}},_id:0})
-        .then(result => {
-            response.status(200).json(result)
-        })
-        .catch(error => next(error));
-}
+exports.getClinic = function (request, response, next) {
+  Clinic.find({})
+    .then((result) => {
+      response.status(200).json(result);
+    })
+    .catch((error) => next(error));
+};
+exports.getService = function (request, response, next) {
+  Clinic.findOne({ _id: request.params.id }, {services})
+    .then((result) => {
+      response.status(200).json(result);
+    })
+    .catch((error) => next(error));
+};
 
 ////////////////////////////////////////POST//////////////////////////////////////////////////////////
 exports.createClinic = (request, response, next) => {
-    //validation result
-    let clinicObject = new Clinic({
-        location: request.body.location,
-        services: request.body.services,
-    })
+  //validation result
+  let clinicObject = new Clinic({
+    location: request.body.location,
+    services: request.body.services,
+  });
 
-    clinicObject.save()
-        .then(result => {
-            response.status(201).json({ message: "added" })
-        }).catch(error => console.log(error))
-}
+  clinicObject
+    .save()
+    .then((result) => {
+      response.status(201).json({ message: "added" });
+    })
+    .catch((error) => console.log(error));
+};
