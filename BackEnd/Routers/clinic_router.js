37e6576b75e1ee2a633ed1cd44049f39f17aae3c
RandaHadad <randahada99@gmail.com>
2022-03-24 15:08:59 +0200

    prescreption

--- BackEnd/Routers/clinic_router.js
+++ BackEnd/Routers/clinic_router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require("./../Controllers/clinic_controller")
 
 router.get("/all", controller.getClinic);
-router.get("/service/:id", controller.getService);
+router.get("/service/:id", controller.getServiceName);
 
 router.post("", controller.createClinic);
 
