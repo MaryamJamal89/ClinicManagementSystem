eb14aa8536a167ddd6e78283d97da0f20094d841
Mostafa Refaat <mostafarefaat.eladl@gmail.com>
2022-03-27 21:02:54 +0200

    still need edit to recep add

--- Angular/src/app/receptionist/add-receptionist/add-receptionist.component.ts
+++ Angular/src/app/receptionist/add-receptionist/add-receptionist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReceptionistService } from 'src/app/receptionist.service';
+import { Receptionist } from 'src/app/_models/receptionist';
 
 @Component({
   selector: 'pm-add-receptionist',
@@ -7,12 +9,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-receptionist.component.css', '../../../assets/css/adminlte.min.css']
 })
 export class AddReceptionistComponent implements OnInit {
-
-  constructor(public router:Router) { }
+  newRecep:Receptionist= new Receptionist("62345f2086e4b9494d6237a4","","","");
+  permission:string="";
+  constructor(public router:Router,public recSrv:ReceptionistService) { }
 
   ngOnInit(): void {
   }
 
+  selectPermission(event: any){
+    if(event.target.value)
+    this.permission = event.target.value;
+  }
+
+  addRecep(){
+    this.newRecep.permissions=this.permission
+    this.recSrv.addRecep(this.newRecep).subscribe({
+      next:a=>{this.newRecep=a}
+    })
+  }
+ 
   backToDash(){
     this.router.navigateByUrl(`/receptionist`);
   }
