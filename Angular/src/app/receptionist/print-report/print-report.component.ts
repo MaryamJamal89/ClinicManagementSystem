36b24fea0d5b1ba57fb6f01ce863bf2e46bc63ba
Maryam El-Aiat <maryamalayaat@gmail.com>
2022-03-27 18:34:40 +0200

    edit some routes

--- Angular/src/app/receptionist/print-report/print-report.component.ts
+++ Angular/src/app/receptionist/print-report/print-report.component.ts
@@ -47,11 +47,11 @@ export class PrintReportComponent implements OnInit {
           data: [60, 40],
           // TODO: Calc gender rate
           backgroundColor: [
-            'rgba(153, 102, 255, 0.8)',
-            'rgba(75, 192, 192, 0.8)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
             'rgba(255, 206, 86, 0.2)',
-            'rgba(54, 162, 235, 0.8)',
-            'rgba(255, 99, 132, 0.8)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 99, 132, 0.3)',
             'rgba(255, 159, 64, 0.2)'
           ],
           borderColor: [
